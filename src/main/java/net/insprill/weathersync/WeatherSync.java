1f0d978b0817f82989027a73a8a15782e5885b78
Pierce Thompson <insprill@gmail.com>
2022-07-15 03:00:22 -0400

    Update WeatherSync to update the weather on the configured basis

--- src/main/java/net/insprill/weathersync/WeatherSync.java
+++ src/main/java/net/insprill/weathersync/WeatherSync.java
@@ -1,18 +1,42 @@
 package net.insprill.weathersync;
 
+import net.insprill.weathersync.weather.WeatherService;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public final class WeatherSync extends JavaPlugin {
 
+    private static final int MIN_UPDATE_RATE = 1200;
+
+    private WeatherService weatherService;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
 
+        this.weatherService = new WeatherService(this, getConfig().getString("api-key"));
+
+        long updateRate = getConfig().getLong("update-rate");
+        updateRate = Math.max(MIN_UPDATE_RATE, updateRate);
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, this::updateAllWeather, 0, updateRate);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    /**
+     * Updates the weather on all configured worlds.
+     */
+    private void updateAllWeather() {
+        for (String worldName : getConfig().getConfigurationSection("worlds").getKeys(false)) {
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                getLogger().log(Level.SEVERE, "Failed to find world by name {}!", worldName);
+                continue;
+            }
+            String location = getConfig().getString("worlds." + worldName + ".location");
+            this.weatherService.fetchAndUpdate(world, location);
+        }
     }
 
 }
