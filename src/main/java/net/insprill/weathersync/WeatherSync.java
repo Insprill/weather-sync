d0cad71853bba71f3e01e8892bcd5f11be3f984c
Pierce Thompson <insprill@gmail.com>
2022-07-15 00:55:32 -0400

    Initial commit

---
+++ src/main/java/net/insprill/weathersync/WeatherSync.java
@@ -0,0 +1,18 @@
+package net.insprill.weathersync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WeatherSync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
